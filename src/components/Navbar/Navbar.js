40e9ff57b308cbdeb48678ad0f60bd20fddc6d6b
Vithujan99 <vithujan99@hotmail.com>
2024-11-11 15:01:35 +0100

    Home Hero Section Animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 import { NavLink, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
@@ -19,9 +20,77 @@ const Navbar = () => {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+  const navVariants = {
+    hidden: { opacity: 0, y: -100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1.7, ease: "easeIn", delay: -1.5 },
+    },
+  };
+  const logoVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5, ease: "easeIn", delay: 0.4 },
+    },
+  };
 
+  const menuItemVariantsHome = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+        delay: 0.7,
+      },
+    },
+  };
+  const menuItemVariantsAbout = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+        delay: 0.9,
+      },
+    },
+  };
+  const menuItemVariantsCareer = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+        delay: 1.1,
+      },
+    },
+  };
+  const menuItemVariantsContact = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+        delay: 1.3,
+      },
+    },
+  };
   return (
-    <nav className={scroll ? "moving-navbar" : "navbar"}>
+    <motion.nav
+      className={scroll ? "moving-navbar" : "navbar"}
+      variants={navVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <div className="navbar-container">
         <div
           className={
@@ -35,6 +104,7 @@ const Navbar = () => {
           >
             Home
           </NavLink>
+
           <NavLink
             className={pathname === "/FamilieSieger" ? "active-link" : "link"}
             to="/FamilieSieger"
@@ -57,7 +127,10 @@ const Navbar = () => {
             Kontakt
           </NavLink>
         </div>
-        <div className="logo-holder">REWE</div>
+
+        <motion.div className="logo-holder" variants={logoVariants}>
+          REWE
+        </motion.div>
         {/* <img
           className="logo-holder"
           src={siegerLogo}
@@ -65,29 +138,44 @@ const Navbar = () => {
         />*/}
 
         <div className="link-holder">
-          <NavLink className={pathname === "/" ? "active-link" : "link"} to="/">
-            Home
-          </NavLink>
-          <NavLink
-            className={pathname === "/FamilieSieger" ? "active-link" : "link"}
-            to="/FamilieSieger"
-          >
-            Über uns
-          </NavLink>
-          <NavLink
-            className={pathname === "/Karriere" ? "active-link" : "link"}
-            to="/Karriere"
-          >
-            Karriere
-          </NavLink>
-          <NavLink
-            className={pathname === "/Kontakt" ? "active-link" : "link"}
-            to="/Kontakt"
-          >
-            Kontakt
-          </NavLink>
+          <motion.div variants={menuItemVariantsHome}>
+            <NavLink
+              to="/"
+              className={pathname === "/" ? "active-link" : "link"}
+            >
+              Home
+            </NavLink>
+          </motion.div>
+          <motion.div variants={menuItemVariantsAbout}>
+            <NavLink
+              className={pathname === "/FamilieSieger" ? "active-link" : "link"}
+              to="/FamilieSieger"
+            >
+              Über uns
+            </NavLink>
+          </motion.div>
+          <motion.div variants={menuItemVariantsCareer}>
+            <NavLink
+              className={pathname === "/Karriere" ? "active-link" : "link"}
+              to="/Karriere"
+            >
+              Karriere
+            </NavLink>
+          </motion.div>
+          <motion.div variants={menuItemVariantsContact}>
+            <NavLink
+              className={pathname === "/Kontakt" ? "active-link" : "link"}
+              to="/Kontakt"
+            >
+              Kontakt
+            </NavLink>
+          </motion.div>
         </div>
-        <div className="menu-icon" onClick={toggleMenu}>
+        <motion.div
+          className="menu-icon"
+          onClick={toggleMenu}
+          variants={menuItemVariantsHome}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -102,9 +190,9 @@ const Navbar = () => {
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
-        </div>
+        </motion.div>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
--- src/routes/Home/Hero/HomeHero.js
+++ src/routes/Home/Hero/HomeHero.js
@@ -2,19 +2,94 @@ import React from "react";
 import "./HomeHero.css";
 import heroimg from "../../../images/rewe_von_außen.jpg";
 import siegerLogo from "../../../images/sieger_logo.jpg";
+import { motion } from "framer-motion";
 
 const HomeHero = () => {
+  const heroImgVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5 },
+    },
+  };
+  const heroTextContentVariants = {
+    hidden: {
+      y: "100%", // Starts off-screen below
+    },
+    visible: {
+      y: 0, // Moves up into view
+      transition: {
+        duration: 1.5, // Similar to GSAP's duration
+        ease: [0.2, 1, 0.2, 1], // Ease similar to power4.out
+        delay: 1.4,
+      },
+    },
+  };
+  const heroTextTielVariants = {
+    hidden: {
+      opacity: 0,
+      scale: 0.9,
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 8,
+        mass: 0.5,
+        duration: 0.8,
+        delay: 1.8,
+      },
+    },
+  };
+  const heroTextImgVariants = {
+    hidden: {
+      opacity: 0,
+      x: 40,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+        mass: 0.8,
+        duration: 0.8,
+        delay: 2,
+      },
+    },
+  };
   return (
     <div id="homehero">
-      <img id="homehero-img" src={heroimg} alt="Rewe Filiale von Außen" />
-      <div className="hero-text-content">
+      <motion.img
+        id="homehero-img"
+        src={heroimg}
+        alt="Rewe Filiale von Außen"
+        variants={heroImgVariants}
+        initial="hidden"
+        animate="visible"
+      />
+      <motion.div
+        className="hero-text-content"
+        variants={heroTextContentVariants}
+        initial="hidden"
+        animate="visible"
+      >
         <div className="container">
           <div className="hero-text-content-center">
-            <h1>Herzlich Willkommen zu</h1>
-            <img src={siegerLogo} alt="Logo der Rewe Filiale" />
+            <motion.h1 variants={heroTextTielVariants}>
+              Herzlich Willkommen zu
+            </motion.h1>
+            <motion.img
+              src={siegerLogo}
+              alt="Logo der Rewe Filiale"
+              variants={heroTextImgVariants}
+            />
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
