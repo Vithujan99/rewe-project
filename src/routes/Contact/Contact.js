b63a34c1b23f6bf73c74bb15feb40abf4078c568
Vithujan99 <vithujan99@hotmail.com>
2024-11-22 15:44:57 +0100

    Kontakt Form Start

--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ContactHero from "./ContactHero/ContactHero";
 import ContactInfo from "./ContactInfo/ContactInfo";
+import ContactForm from "./ContactForm/ContactForm";
 
 const Contact = () => {
   return (
     <div>
       <ContactHero></ContactHero>
       <ContactInfo></ContactInfo>
+      <ContactForm></ContactForm>
     </div>
   );
 };
---
+++ src/routes/Contact/ContactForm/ContactForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./ContactForm.css";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, subject, message } = formData;
+    const mailtoLink = `mailto:vithujan99@hotmail.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(
+      `Name: ${name}\nE-Mail: ${email}\n\nNachricht:\n${message}`
+    )}`;
+    window.location.href = mailtoLink;
+  };
+  return (
+    <div className="contactform">
+      <div className="container">
+        <h2>Wir sind für Sie da</h2>
+        <form onSubmit={handleSubmit} className="cf-holder">
+          <div className="cf-il-holder">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="cf-il-holder">
+            <label for="email">E-Mail</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="cf-il-holder">
+            <label for="subject">Betreff</label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="cf-il-holder">
+            <label for="message">Nachricht</label>
+            <textarea
+              name="message"
+              id="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+              rows="5"
+            />
+          </div>
+
+          <button type="submit">Abschicken</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
