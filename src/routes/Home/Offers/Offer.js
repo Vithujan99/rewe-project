ecb2cce42cd47c8c28198425085a4547f7da5cff
Vithujan99 <vithujan99@hotmail.com>
2024-12-09 09:52:31 +0100

    added Prospekt for Mobiel

--- src/routes/Home/Offers/Offer.js
+++ src/routes/Home/Offers/Offer.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Offer.css";
 import pdfUrl from "../../../assets/Endt_KW50_24_web.pdf";
 
 const Offer = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Check the window size when the component mounts or the window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 800); // Set isMobile to true if screen width is less than 800px
+    };
+
+    handleResize(); // Check the size on initial load
+    window.addEventListener("resize", handleResize); // Listen for window resize events
+
+    return () => window.removeEventListener("resize", handleResize); // Cleanup on component unmount
+  }, []);
+
   return (
     <div className="home-offer-section">
       <h2>Aktuelle Angebote</h2>
 
-      <embed
-        className="prospekt-container"
-        src={`${pdfUrl}#zoom=33`}
-        type="application/pdf"
-      />
+      {/* Embed PDF - Only show if screen width is >= 800px */}
+      {!isMobile && (
+        <embed
+          className="prospekt-container"
+          src={`${pdfUrl}#zoom=44`}
+          type="application/pdf"
+          width="100%"
+          height="600px"
+          alt="PDF Preview"
+        />
+      )}
+
+      {/* Download Button - Only show if screen width is < 800px */}
+      {isMobile && (
+        <div className="download-container">
+          <a
+            href={pdfUrl}
+            target="_blank" // Open PDF in a new tab
+            rel="noopener noreferrer" // For security reasons
+            className="prospekt-open-button"
+          >
+            Prospekt im neuen Fenster öffnen
+          </a>
+        </div>
+      )}
     </div>
   );
 };
