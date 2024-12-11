0d1161076dbc556a0ddced0f183aa3877bfd7355
Vithujan99 <vithujan99@hotmail.com>
2024-12-11 15:56:49 +0100

    Chrome pdf bug Solution one

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeHero from "./Hero/HomeHero";
 import ReweApp from "./ReweApp/ReweApp";
 import Offer from "./Offers/Offer";
 
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Setzt die Scroll-Position an den Anfang
+  }, []);
   return (
     <div>
       <HomeHero></HomeHero>
