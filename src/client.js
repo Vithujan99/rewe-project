fa3142ff48ea6d2ad0c1c1f0dd180845ba37f651
Vithujan99 <vithujan99@hotmail.com>
2024-12-21 12:08:30 +0100

    env names changed

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 // contentfulClient.js
 import { createClient } from "contentful";
-const cID = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
-const cAT = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN;
+const cID = process.env.CONTENTFUL_SPACE_ID;
+const cAT = process.env.CONTENTFUL_ACCESS_TOKEN;
 
 const client = createClient({
   space: cID, // Dein Contentful Space ID
